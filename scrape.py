8fbafa59276cdb86d0442df6e8f92700025c1952
code-guru-007 <py.guru.007@gmail.com>
2025-01-17 05:27:29 +0900

    exception handle

--- scrape.py
+++ scrape.py
@@ -17,54 +17,63 @@ driver = Driver(uc=True)
 
 def ScrapeList(start = 2010, end = 2015):
     for year in range(start, end, 1):
-        driver.get(f'{baseUrl}/anno/{year}')
-        current_page = 0
-        driver.get(f'{baseUrl}/{current_page}')
-        
-        card = driver.find_elements(By.XPATH, "//div[starts-with(@id, 'heading_')]")
-        # while len(card):
-        while current_page < 2:
-            # for i in range(len(card)):
-            for i in range(1):           
-                href = card[i].find_element(By.CSS_SELECTOR, 'p > a').get_attribute("href")            
-                parsed_url = urlparse(href)
-                query_params = parse_qs(parsed_url.query)
-                date = query_params.get('atto.dataPubblicazioneGazzetta', [None])[0]
-                
-                #check if current file has been downloaded
-                download_dir = f'{os.getcwd()}\\download'
-                pdf_files = [file for file in os.listdir(download_dir) if file.endswith('.pdf')]
-                match = any(file.startswith(date) for file in pdf_files)
-                if match:
-                    print(date)
-                    continue
-                
-                code = query_params.get('atto.codiceRedazionale', [None])[0]
-                card[i].find_element(By.CSS_SELECTOR, 'p > a').click()
-                driver.get(f'https://www.normattiva.it/atto/vediMenuExport?atto.dataPubblicazioneGazzetta={date}&atto.codiceRedazionale={code}&currentSearch=')
-                pdf_button = driver.find_element(By.NAME, 'downloadPdf')
-                pdf_button.click()
-                driver.switch_to.window(driver.window_handles[1])
-                element = driver.find_element(By.TAG_NAME, 'body')
-                action = ActionChains(driver)
-                action.move_to_element(element).click().perform()
-                
-                pyautogui.hotkey('ctrl', 's')
-                time.sleep(1)
-                pyautogui.write(f'{download_dir}\\{date}.pdf')
-                time.sleep(1)
-                pyautogui.hotkey('enter')
-                time.sleep(1)
-
-                driver.close()
-                driver.switch_to.window(driver.window_handles[0])
-                driver.back()
-                driver.back()
-            current_page += 1
+        try:
+            driver.get(f'{baseUrl}/anno/{year}')
+            current_page = 0
             driver.get(f'{baseUrl}/{current_page}')
-            time.sleep(2)
+            
             card = driver.find_elements(By.XPATH, "//div[starts-with(@id, 'heading_')]")
-        time.sleep(1)
+            # while len(card):
+            while current_page < 2:
+                try:
+                    # for i in range(len(card)):
+                    for i in range(1):
+                        try:           
+                            href = card[i].find_element(By.CSS_SELECTOR, 'p > a').get_attribute("href")            
+                            parsed_url = urlparse(href)
+                            query_params = parse_qs(parsed_url.query)
+                            date = query_params.get('atto.dataPubblicazioneGazzetta', [None])[0]
+                            
+                            #check if current file has been downloaded
+                            download_dir = f'{os.getcwd()}\\download'
+                            pdf_files = [file for file in os.listdir(download_dir) if file.endswith('.pdf')]
+                            match = any(file.startswith(date) for file in pdf_files)
+                            if match:
+                                print(date)
+                                continue
+                            
+                            code = query_params.get('atto.codiceRedazionale', [None])[0]
+                            card[i].find_element(By.CSS_SELECTOR, 'p > a').click()
+                            driver.get(f'https://www.normattiva.it/atto/vediMenuExport?atto.dataPubblicazioneGazzetta={date}&atto.codiceRedazionale={code}&currentSearch=')
+                            pdf_button = driver.find_element(By.NAME, 'downloadPdf')
+                            pdf_button.click()
+                            driver.switch_to.window(driver.window_handles[1])
+                            element = driver.find_element(By.TAG_NAME, 'body')
+                            action = ActionChains(driver)
+                            action.move_to_element(element).click().perform()
+                            
+                            pyautogui.hotkey('ctrl', 's')
+                            time.sleep(1)
+                            pyautogui.write(f'{download_dir}\\{date}.pdf')
+                            time.sleep(1)
+                            pyautogui.hotkey('enter')
+                            time.sleep(1)
+
+                            driver.close()
+                            driver.switch_to.window(driver.window_handles[0])
+                            driver.back()
+                            driver.back()
+                        except:
+                            pass
+                    current_page += 1
+                    driver.get(f'{baseUrl}/{current_page}')
+                    time.sleep(2)
+                    card = driver.find_elements(By.XPATH, "//div[starts-with(@id, 'heading_')]")
+                except:
+                    pass
+            time.sleep(1)
+        except:
+            pass
 
     driver.close()    
 
--- upload.py
+++ upload.py
@@ -8,14 +8,17 @@ def UploadPdf():
     PASSWORD = "aoewiuyrfpqiu34jf209i3f4"
     download_dir = f'{os.getcwd()}\\download'
     session = FTP(SERVER, USERNAME, PASSWORD)
+    files = FTP.nlst(session)  # List the files in the current directory
 
     for f in os.listdir(download_dir):
         print(f)
-        files = FTP.nlst(session)  # List the files in the current directory
         if f in files:
             print(f"{f} exists on the server.")
         else:
-            file = open(f'{download_dir}\\{f}', 'rb')
-            session.storbinary(f'STOR {f}', file)     # send the file
-            file.close()                                    # close file and FTP
+            try:
+                file = open(f'{download_dir}\\{f}', 'rb')
+                session.storbinary(f'STOR {f}', file)     # send the file
+                file.close()                                    # close file and FTP
+            except:
+                print(f"Error occur!   :   {f}")
     session.quit()
